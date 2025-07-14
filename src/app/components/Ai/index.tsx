7824f5eadf5f10956b8b3cc7e3b1460468c843c7
Cleison Carlos <cleisonspider@yahoo.com.br>
2025-07-14 10:20:08 -0300

    updated ( temoção das noticias do blog)

---
+++ src/app/components/Ai/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Ai() {
+  return (
+    <div className='container'>
+
+
+<div className="row py-5">
+
+    <div className="col-lg-4">
+        <img src="/ai.png" className='img-fluid' alt='Inteligência artificial'/>
+    </div>
+
+    <div className="col-lg-8">
+
+
+  <h2 className='fw-bold display-3'><span className='text-white h2 fw-bold'>Turbine seus sistemas com <br /> </span><span className='text-red'>Inteligência Artificial</span></h2>
+<p className='text-white'>Melhore a eficiência dos fluxos de trabalho e impulsione o crescimento da sua empresa com a utilização de tecnologia de Inteligência Artificial.</p>
+<p className='text-white'>A inteligência artificial transforma dados em decisões estratégicas, impulsionando a inovação, a eficiência e a competitividade das empresas no mercado atual.</p>
+ 
+ 
+    </div>
+</div>
+
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,20 @@
 "use client"
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Ai from "./components/Ai";
 import Hero from "./components/Hero";
 import MarqueeIcons from "./components/MarqueeIcons";
 import MyServices from "./components/MyServices";
 import Projects from "./components/Projects";
-import News from "./components/News";
-
+//import News from "./components/News";
 
 export default function Home() {
-
   return (
-    <>
-   
-        <Hero />
-        <MarqueeIcons />
-    
+    <>  
+      <Hero />
+      <MarqueeIcons />  
+      <Ai/>  
       <MyServices />
       <Projects />
-      <News />
- 
+      {/* <News /> */} 
     </>
-
   );
 }
