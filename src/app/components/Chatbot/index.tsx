79481c9e5208348931822846d593b2dc32fb5246
Cleison Carlos <cleisoncarlos@hotmail.com>
2025-07-31 21:02:19 -0300

    update chatbot

--- src/app/components/Chatbot/index.tsx
+++ src/app/components/Chatbot/index.tsx
@@ -1,23 +1,19 @@
 'use client';
 import { Bubble } from "@typebot.io/nextjs";
 
-const App = () => {
+const Chatbot = () => {
   return (
     <Bubble
       typebot="br-cleisoncarlos-com"
       previewMessage={{
-        message: "Posso te ajudar?",
+        message: "Como posso te ajudar?",
         autoShowDelay: 2000,
-        avatarUrl: "https://github.com/cleisoncarlos.png",
+        avatarUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSh7Ka3dFVZHkCbRSFCPJiaEc75jcAqGCgh2A&s",
       }}
-      theme={{
-        button: {
-          backgroundColor: "#128c7e",
-          customIconSrc:
-            "data:image/svg+xml;utf8,<svg fill='%23ffffff' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 640 512'><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2023 Fonticons, Inc. --><path d='M320 0c17.7 0 32 14.3 32 32V96H472c39.8 0 72 32.2 72 72V440c0 39.8-32.2 72-72 72H168c-39.8 0-72-32.2-72-72V168c0-39.8 32.2-72 72-72H288V32c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H208zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H304zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16H400zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224H64V416H48c-26.5 0-48-21.5-48-48V272c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48H576V224h16z'/></svg>",
-          size: "large",
-        },
-      }}
+      theme={{ button: { backgroundColor: "#128c7e", size: "large" } }}
     />
   );
 };
+
+export default Chatbot
