6ce65b81a0dda16dd77f051b37590591e2ce7f1b
Cleison Carlos <cleisoncarlos@hotmail.com>
2025-08-02 23:03:34 -0300

    mais teste

--- src/app/components/Chatbot/index.tsx
+++ src/app/components/Chatbot/index.tsx
@@ -1,30 +1,19 @@
 'use client';
 
-import { Bubble } from '@typebot.io/react';
+'use client';
+
+import { Typebot } from '@typebot.io/react';
 
 const Chatbot = () => {
   return (
-    <Bubble
+    <Typebot
       typebot="br-cleisoncarlos-com"
-      customBaseUrl="https://chatbot.cleisoncarlos.com" // 👈 Define sua instância self-hosted!
-      previewMessage={{
-        message: "Como posso te ajudar?",
-        autoShowDelay: 2000,
-        avatarUrl: 'https://static.vecteezy.com/system/resources/previews/034/211/480/non_2x/ai-chatbots-icon-illustration-vector.jpg',
-      }}
-      theme={{
-        button: { backgroundColor: '#af1010ff', size: 'large' },
-      }}
-      prefilledVariables={{
-        'Current URL': 'https://br.cleisoncarlos.com',
-        'User name': 'John Doe',
-      }}
+      customBaseUrl="https://chatbot.cleisoncarlos.com"
+      style={{ width: '100%', height: '600px' }}
     />
   );
 };
 
-
 export default Chatbot;
-
 
 
